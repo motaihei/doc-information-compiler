183ea9039da209417549a9d5ac16fe9a19054d76
moai\moai_dev <moai.develop@gmail.com>
2023-04-10 04:38:36 +0900

    出力データを調整

--- src/create_md.py
+++ src/create_md.py
@@ -1,18 +1,32 @@
 import subprocess
+import datetime
 
-# Call PowerShell script to get open Excel file addresses
+# addores_output.ps1を実行して、開いているExcelファイルのアドレスを取得する
 cmd = 'powershell.exe -ExecutionPolicy RemoteSigned -File addres_output.ps1'
 result = subprocess.run(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
 
-# Extract addresses from PowerShell output
+# 取得したアドレスをマークダウン形式でリンクを持ったテキストにする
+# PowerShellの出力はshift-jisなので、decodeで文字コードを変換する
 addresses = result.stdout.decode('shift-jis').split()
-
-# Create markdown text with file names and addresses
 markdown_text = ''
 for address in addresses:
     file_name = address.split('\\')[-1]
     markdown_text += f"[{file_name}](file:///{address})\n"
 
-# Write markdown text to file
-with open('file_addresses.md', 'w', encoding='shift-jis') as file:
+# 'yyyy-mm-dd'形式で日付を文字列で初期化する
+date = datetime.date.today().strftime('%Y-%m-%d')
+
+# マークダウン形式でリンクを持ったテキストをファイルに書き込む
+with open(f'../output/file_addresses_{date}.md', 'w', encoding='shift-jis') as file:
+    # 見出しは日付にする
+    file.write(f'# {date}\n')
     file.write(markdown_text)
+
+# 出力先を標準出力にする
+print(markdown_text)
+
+
+
+
+
+
